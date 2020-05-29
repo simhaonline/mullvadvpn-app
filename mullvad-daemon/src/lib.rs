9bf3e384781464a13cb71f738e845b7793785132
David Lönnhager <david.l@mullvad.net>
2020-05-29 10:04:12 +0200

    Keep split tunnel object in Daemon

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -52,7 +52,7 @@ use std::{
     sync::{mpsc as sync_mpsc, Arc, Weak},
     time::Duration,
 };
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", windows))]
 use talpid_core::split_tunnel;
 use talpid_core::{
     mpsc::Sender,
@@ -94,7 +94,7 @@ pub enum Error {
     #[error(display = "Unable to load account history with wireguard key cache")]
     LoadAccountHistory(#[error(source)] account_history::Error),
 
-    #[cfg(target_os = "linux")]
+    #[cfg(any(target_os = "linux", windows))]
     #[error(display = "Unable to initialize split tunneling")]
     InitSplitTunneling(#[error(source)] split_tunnel::Error),
 
@@ -465,6 +465,8 @@ pub struct Daemon<L: EventListener> {
     last_generated_bridge_relay: Option<Relay>,
     app_version_info: AppVersionInfo,
     shutdown_callbacks: Vec<Box<dyn FnOnce()>>,
+    #[cfg(windows)]
+    split_tunnel: split_tunnel::SplitTunnel,
     /// oneshot channel that completes once the tunnel state machine has been shut down
     tunnel_state_machine_shutdown_signal: oneshot::Receiver<()>,
     cache_dir: PathBuf,
@@ -593,6 +595,9 @@ where
         // Attempt to download a fresh relay list
         relay_selector.update().await;
 
+        #[cfg(windows)]
+        let split_tunnel = split_tunnel::SplitTunnel::new().map_err(Error::InitSplitTunneling)?;
+
         let mut daemon = Daemon {
             tunnel_command_tx,
             tunnel_state: TunnelState::Disconnected,
@@ -616,6 +621,8 @@ where
             last_generated_bridge_relay: None,
             app_version_info,
             shutdown_callbacks: vec![],
+            #[cfg(windows)]
+            split_tunnel,
             tunnel_state_machine_shutdown_signal,
             cache_dir,
         };
