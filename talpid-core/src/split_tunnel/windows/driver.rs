14f9a2034e0e5ab26d5928377833d5bfca50e0b3
David Lönnhager <david.l@mullvad.net>
2020-09-09 12:05:32 +0200

    Add basic driver types

--- talpid-core/src/split_tunnel/windows/driver.rs
+++ talpid-core/src/split_tunnel/windows/driver.rs
@@ -1,9 +1,50 @@
-use std::{
-    io, mem,
-    os::windows::{ffi::OsStringExt, io::RawHandle},
-    ptr,
+use std::{io, mem, os::windows::io::RawHandle, ptr};
+use winapi::um::{
+    ioapiset::DeviceIoControl,
+    winioctl::{FILE_ANY_ACCESS, METHOD_BUFFERED, METHOD_NEITHER},
 };
-use winapi::um::ioapiset::DeviceIoControl;
+
+const DRIVER_SYMBOLIC_NAME: &str = "\\\\.\\MULLVADSPLITTUNNEL";
+const ST_DEVICE_TYPE: u32 = 0x8000;
+
+const fn ctl_code(device_type: u32, function: u32, method: u32, access: u32) -> u32 {
+    device_type << 16 | access << 14 | function << 2 | method
+}
+
+#[repr(u32)]
+#[allow(dead_code)]
+enum DriverIoctlCode {
+    Initialize = ctl_code(ST_DEVICE_TYPE, 1, METHOD_NEITHER, FILE_ANY_ACCESS),
+    DequeEvent = ctl_code(ST_DEVICE_TYPE, 2, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    RegisterProcesses = ctl_code(ST_DEVICE_TYPE, 3, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    RegisterIpAddresses = ctl_code(ST_DEVICE_TYPE, 4, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    GetIpAddresses = ctl_code(ST_DEVICE_TYPE, 5, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    SetConfiguration = ctl_code(ST_DEVICE_TYPE, 6, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    GetConfiguration = ctl_code(ST_DEVICE_TYPE, 7, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    ClearConfiguration = ctl_code(ST_DEVICE_TYPE, 8, METHOD_NEITHER, FILE_ANY_ACCESS),
+    GetState = ctl_code(ST_DEVICE_TYPE, 9, METHOD_BUFFERED, FILE_ANY_ACCESS),
+    QueryProcess = ctl_code(ST_DEVICE_TYPE, 10, METHOD_BUFFERED, FILE_ANY_ACCESS),
+}
+
+#[derive(Debug, PartialEq)]
+#[repr(u32)]
+#[allow(dead_code)]
+enum DriverState {
+    // Default state after being loaded.
+    None = 0,
+    // DriverEntry has completed successfully.
+    // Basically only driver and device objects are created at this point.
+    Started = 1,
+    // All subsystems are initialized.
+    Initialized = 2,
+    // User mode has registered all processes in the system.
+    Ready = 3,
+    // IP addresses are registered.
+    // A valid configuration is registered.
+    Engaged = 4,
+    // Driver is unloading.
+    Terminating = 5,
+}
 
 /// Send an IOCTL code to the given device handle.
 /// `input` specifies an optional buffer to send.
