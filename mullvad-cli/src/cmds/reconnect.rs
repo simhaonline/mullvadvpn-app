4e4ee10d80e90c3dc4f6ddfef2fd35b642fc4c0f
Oskar Nyberg <oskar@mullvad.net>
2020-10-01 13:22:02 +0200

    Add --wait argument to disconnect and reconnect commands

--- mullvad-cli/src/cmds/reconnect.rs
+++ mullvad-cli/src/cmds/reconnect.rs
@@ -1,4 +1,11 @@
-use crate::{new_rpc_client, Command, Result};
+use crate::{format, new_rpc_client, state, Command, Result};
+use mullvad_management_interface::types::{
+    tunnel_state::{
+        Error as TunnelError,
+        State::{Connected, Disconnected, Error},
+    },
+    ErrorState,
+};
 use talpid_types::ErrorExt;
 
 pub struct Reconnect;
@@ -10,14 +17,52 @@ impl Command for Reconnect {
     }
 
     fn clap_subcommand(&self) -> clap::App<'static, 'static> {
-        clap::SubCommand::with_name(self.name()).about("Command the client to reconnect")
+        clap::SubCommand::with_name(self.name())
+            .about("Command the client to reconnect")
+            .arg(
+                clap::Arg::with_name("wait")
+                    .long("wait")
+                    .short("w")
+                    .help("Wait until reconnected before exiting"),
+            )
     }
 
-    async fn run(&self, _: &clap::ArgMatches<'_>) -> Result<()> {
+    async fn run(&self, matches: &clap::ArgMatches<'_>) -> Result<()> {
         let mut rpc = new_rpc_client().await?;
+
+        let status_listen_handle = if matches.is_present("wait") {
+            let state = rpc.get_tunnel_state(()).await?.into_inner();
+            format::print_state(&state);
+
+            match state.state {
+                Some(Disconnected(_)) => None,
+                Some(Error(TunnelError {
+                    error_state:
+                        Some(ErrorState {
+                            blocking_error: Some(_),
+                            ..
+                        }),
+                })) => None,
+                _ => Some(
+                    state::state_listen(&mut rpc, |state| match state {
+                        Connected(_) => false,
+                        Error(_) => false,
+                        _ => true,
+                    })
+                    .await?,
+                ),
+            }
+        } else {
+            None
+        };
+
         if let Err(e) = rpc.reconnect_tunnel(()).await {
             eprintln!("{}", e.display_chain());
         }
+        if let Some(handle) = status_listen_handle {
+            handle.await.expect("Failed to listen to status updates");
+        }
+
         Ok(())
     }
 }
